f5ce5144f28f876ff67434dfdde3b6faac26112c
chin@fx <chin@fx>
2016-10-05 19:36:37 +0800

    add missing folder form dlib

---
+++ tools/imglab/build/CMakeFiles/imglab.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/chin/Git/DLIB/tools/imglab/src/cluster.cpp" "/home/chin/Git/DLIB/tools/imglab/build/CMakeFiles/imglab.dir/src/cluster.cpp.o"
+  "/home/chin/Git/DLIB/tools/imglab/src/common.cpp" "/home/chin/Git/DLIB/tools/imglab/build/CMakeFiles/imglab.dir/src/common.cpp.o"
+  "/home/chin/Git/DLIB/tools/imglab/src/convert_idl.cpp" "/home/chin/Git/DLIB/tools/imglab/build/CMakeFiles/imglab.dir/src/convert_idl.cpp.o"
+  "/home/chin/Git/DLIB/tools/imglab/src/convert_pascal_v1.cpp" "/home/chin/Git/DLIB/tools/imglab/build/CMakeFiles/imglab.dir/src/convert_pascal_v1.cpp.o"
+  "/home/chin/Git/DLIB/tools/imglab/src/convert_pascal_xml.cpp" "/home/chin/Git/DLIB/tools/imglab/build/CMakeFiles/imglab.dir/src/convert_pascal_xml.cpp.o"
+  "/home/chin/Git/DLIB/tools/imglab/src/main.cpp" "/home/chin/Git/DLIB/tools/imglab/build/CMakeFiles/imglab.dir/src/main.cpp.o"
+  "/home/chin/Git/DLIB/tools/imglab/src/metadata_editor.cpp" "/home/chin/Git/DLIB/tools/imglab/build/CMakeFiles/imglab.dir/src/metadata_editor.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/chin/Git/DLIB/tools/imglab/build/dlib_build/CMakeFiles/dlib.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/chin/Git/DLIB/dlib/.."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
